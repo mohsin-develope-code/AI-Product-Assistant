04da34453eab49d6dc8fe4402ef106736acbf3fd
Mohammad Mohsin <167190542+mohsin-develope-code@users.noreply.github.com>
2025-11-11 15:44:58 +0530

    Update CORS origin to production URL

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 
 
 app.use(cors({ 
-              origin: 'http://localhost:5173',  
+              origin: 'https://ai-product-chatbot.netlify.app',  
               methods: ['GET', 'POST', 'PUT', 'DELETE'],
               credentials: true, 
         }));
